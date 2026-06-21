ad82ed899ef57d8c755bb738d96b4cfc104e2f2d
janithdidula70-sudo <janithdidula70@gmail.com>
2026-06-21 21:41:46 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+const SPREADSHEET_ID = 'AKfycbz159jvD3zy2LNoG8NsmKd622_KQWwzRXUtQ0GMU7lHeDMSxS8BGP-1LZ95Nvy5UDctgQ';
+
+const SHEETS = {
+  TEACHERS: 'Teachers',
+  CLASSES: 'Classes',
+  STUDENTS: 'Students',
+  ATTENDANCE: 'Attendance',
+  PAYMENTS: 'Payments',
+};
